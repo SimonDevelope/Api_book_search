8162d0ff8b72179d376c6abf76961c9dc010468e
SimonDevelope <whwnine@gmail.com>
2021-08-18 23:53:50 +0900

    #4. 추가사항 더 남음 이후 에러수정 및 기능 추가부여

--- src/Components/api.tsx
+++ src/Components/api.tsx
@@ -3,11 +3,17 @@ import axios from "axios";
 import "../Styles/api.scss";
 
 require("dotenv").config();
+interface nextPageData {
+  next?: string;
+}
 
 function Kakao() {
   const [bookList, setBookList] = useState<[]>([]);
+  const [nextList, setNextList] = useState<nextPageData>({ next: undefined });
   const [inputText, setInputText] = useState<string>("");
   const [search, setSearch] = useState<string>("");
+  const [moreData, setMoreData] = useState<boolean>(false);
+
   useEffect(() => {
     if (search.length > 0) {
       bookSearch(search);
@@ -24,16 +30,27 @@ function Kakao() {
           params: {
             query: search,
             sort: "accuracy",
+            page: 1,
             size: 20,
           },
         })
         .then((response) => {
           setBookList(response.data.documents);
           setInputText("");
+          setNextList(response.data.meta.is_end);
+          console.log(nextList);
         });
     } catch (err) {
       console.log(err);
     }
+  };
+
+  const loadMoreData = async () => {
+    await axios.get(nextList.next).then((response) => {
+      const loadedData = response.data.documents;
+      const addUpData = bookList.concat(...loadedData);
+      setBookList(addUpData);
+    });
   };
 
   const onEnter: React.KeyboardEventHandler<HTMLInputElement> = (e) => {
