b7887e64e782297e8ea49e1c767102372c824c88
SimonDevelope <whwnine@gmail.com>
2021-08-21 18:32:45 +0900

    initial setting

--- src/Components/api.tsx
+++ src/Components/api.tsx
@@ -1,17 +1,13 @@
-import React, { useCallback, useEffect, useRef, useState } from "react";
-import axios from "axios";
-import "../Styles/api.scss";
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+import axios from 'axios';
+import '../Styles/api.scss';
 
-require("dotenv").config();
-
-export interface BookProps {
-  is_end: string | boolean | undefined;
-}
+require('dotenv').config();
 
 function Kakao() {
   const [bookList, setBookList] = useState<[]>([]);
-  const [inputText, setInputText] = useState<string>("");
-  const [search, setSearch] = useState<string>("");
+  const [inputText, setInputText] = useState<string>('');
+  const [search, setSearch] = useState<string>('');
   const [page, setPage] = useState<number>(0);
   const [loading, setLoading] = useState<boolean>(false);
   const loader: any = useRef();
@@ -26,13 +22,13 @@ function Kakao() {
     try {
       await setLoading(loading);
       await axios
-        .get("https://dapi.kakao.com/v3/search/book", {
+        .get('https://dapi.kakao.com/v3/search/book', {
           headers: {
             Authorization: `KakaoAK ${process.env.REACT_APP_KAKAO_KEY}`,
           },
           params: {
             query: search,
-            sort: "accuracy",
+            sort: 'accuracy',
             page: page,
             size: 20,
           },
@@ -40,7 +36,7 @@ function Kakao() {
         .then((response) => {
           setBookList(response.data.documents);
           console.log(page);
-          setInputText("");
+          setInputText('');
         });
     } catch (err) {
       console.log(err);
@@ -58,7 +54,7 @@ function Kakao() {
   useEffect(() => {
     const option = {
       root: null,
-      rootMargin: "20px",
+      rootMargin: '20px',
       threshold: 0,
     };
 
@@ -70,7 +66,7 @@ function Kakao() {
   }, [handleObserver]);
 
   const onEnter: React.KeyboardEventHandler<HTMLInputElement> = (e) => {
-    if (e.key === "Enter") {
+    if (e.key === 'Enter') {
       setSearch(inputText);
     }
   };
@@ -97,18 +93,12 @@ function Kakao() {
               <div className="list-inner-wrapper" key={index}>
                 <div className="list-image-wrapper">
                   <a href={list.url}>
-                    <img
-                      src={list.thumbnail}
-                      alt={list.thumbnail}
-                      className="list-image-container"
-                    ></img>
+                    <img src={list.thumbnail} alt={list.thumbnail} className="list-image-container"></img>
                   </a>
                 </div>
                 <div>
                   <div className="list-title-container">{list.title}</div>
-                  <div className="list-price-container">
-                    가격 : {list.price}
-                  </div>
+                  <div className="list-price-container">가격 : {list.price}</div>
                 </div>
               </div>
             );
